0ca39f5c4fc25fbf0956f3af72bf6d79b667d9c1
Azhagu <azhagukarthikeyanb@gmail.com>
2025-02-10 12:16:53 +0530

    Added

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express();
+const port = process.env.PORT;
+
+// middlewares
+app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API working" });
+});
+
+app.listen(port, () => console.log(`Server started`));
